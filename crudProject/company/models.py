6c53158c90f52d8de7c9182116591a647a9b0bcc
albertosdneto <albertosdneto@gmail.com>
2019-10-15 23:05:07 -0300

    Setup database. Implemented basic list of companies

--- crudProject/company/models.py
+++ crudProject/company/models.py
@@ -1,3 +1,19 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 # Create your models here.
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=250)
+    cnpj = models.CharField(max_length=14)
+    addresses = ArrayField(
+        ArrayField(
+            models.CharField(max_length=100, blank=True),
+            size=9,
+        ),
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
