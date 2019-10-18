485d99b2462e8376dca5d557ef4a7102660a643a
albertosdneto <albertosdneto@gmail.com>
2019-10-18 06:46:30 -0300

    Setup basic form to register a new company

--- crudProject/company/views.py
+++ crudProject/company/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from .models import Company
 from django.http import JsonResponse
+from django.shortcuts import render
+from .forms import CompanyForm
+from .models import Company
+
 # Create your views here.
 
 
@@ -16,7 +18,7 @@ def company(request):
 
 
 def getCompanyList(request):
-    if request.method == "GET":  # and request.is_ajax():
+    if request.method == "GET" and request.is_ajax():  # and request.is_ajax():
         try:
             companies = list(Company.objects.all().values())
             data = dict()
@@ -25,4 +27,17 @@ def getCompanyList(request):
             return JsonResponse({"success": False}, status=400)
 
         return JsonResponse(data, status=200)
+    return JsonResponse({"success": False}, status=400)
+
+
+def newCompanytPage(request):
+    form = CompanyForm()
+    return render(request, 'company/new_company.html', {'companyForm': form})
+
+
+def postNewCompany(request):
+    if request.method == "POST":
+        form = CompanyForm(request.POST)
+        form.save()
+        return JsonResponse({"success": True}, status=200)
     return JsonResponse({"success": False}, status=400)
