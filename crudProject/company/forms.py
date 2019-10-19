934b60864f4c70c0b75b7d3c643e8e7ee4bca4eb
albertosdneto <albertosdneto@gmail.com>
2019-10-18 21:09:53 -0300

    Added addres fields to Company model and adjusted form for new company

--- crudProject/company/forms.py
+++ crudProject/company/forms.py
@@ -1,9 +1,17 @@
 from django import forms
-from .models import Company
+from .models import Company, CompanyAddress
 
 
 class CompanyForm(forms.ModelForm):
 
     class Meta:
         model = Company
-        fields = ("name", "cnpj")
+        fields = ['name', 'cnpj', 'line1',
+                  'line2', 'zipCode', 'city', 'state', 'country']
+
+
+class CompanyAddressForm(forms.ModelForm):
+    class Meta:
+        model = CompanyAddress
+        fields = ['addressType', 'line1',
+                  'line2', 'zipCode', 'city', 'state', 'country']
