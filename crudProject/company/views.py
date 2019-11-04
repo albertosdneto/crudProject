830be50ba4899bfbf16ddcf07f1df4dd25421b48
albertosdneto <albertosdneto@gmail.com>
2019-11-04 17:31:23 -0300

    Bug fixes

--- crudProject/company/views.py
+++ crudProject/company/views.py
@@ -8,40 +8,14 @@ from .models import Company, CompanyAddress
 
 
 def home(request):
-    """View to present the home page of the application
-
-    Arguments:
-        request {none} -- none
-
-    Returns:
-        Html Page -- Returns the template for the Home Page
-    """
     return render(request, 'company/home.html')
 
 
 def company(request):
-    """Lists all the companies saved in the database
-
-    Arguments:
-        request {None} -- None
-
-    Returns:
-        Html Page -- Renders the template to show all the companies and 
-                     allows one to search, edit, delete or open to view 
-                     details of a certain company
-    """
     return render(request, 'company/company_list.html')
 
 
 def getCompanyList(request):
-    """Reloads the company list at the company view
-
-    Arguments:
-        request {GET} -- Must be a GET request and ajax.
-
-    Returns:
-        Json -- Returns a Json response containing the list of companies
-    """
     if request.method == "GET" and request.is_ajax():
         try:
             companies = list(Company.objects.all().values())
@@ -56,16 +30,6 @@ def getCompanyList(request):
 
 
 def getCompanyDetails(request, pk):
-    """Returns the details of a company
-
-    Arguments:
-        request {GET} -- This function is called at the company view
-        pk {integer} -- This is the integer corresponding to the id of the company at the database
-
-
-    Returns:
-        Html Page -- Returns the template page with company details
-    """
     company = get_object_or_404(Company, pk=pk)
     addresses = CompanyAddress.objects.filter(
         company=company).order_by('zipCode')
@@ -77,20 +41,45 @@ def getCompanyDetails(request, pk):
     }
     return render(request, 'company/company_details.html', context)
 
+
+def update_company(request, company_pk):
+    company = get_object_or_404(Company, pk=company_pk)
+    company_form = CompanyForm(request.POST or None,
+                               request.FILES or None, instance=company)
+
+    addresses = CompanyAddress.objects.filter(company=company_pk)
+
+    if request.method == "POST" and request.is_ajax():
+        if company_form.is_valid():
+            company_form.save()
+            return JsonResponse({'message': 'Company updated successfully!', 'success': True}, status=200)
+        elif (not company_form.is_valid()):
+            return JsonResponse({'message': 'Error during form validation!', 'success': False}, status=400)
+
+    context = {
+        'company': company,
+        'company_form': company_form,
+        'addresses': addresses,
+    }
+    return render(request, 'company/company_update.html', context)
+
+
 def editCompany(request, pk):
     company = get_object_or_404(Company, pk=pk)
     context = {
         'company': company
     }
     return render(request, 'company/company_edit.html', context)
-    
+
+
 def updateCompanyDetails(request, pk):
     data = dict()
     try:
         company = Company.objects.get(pk=pk)
         company.name = request.POST['name']
+        company
         company.save()
-        
+
         data['message'] = 'Company updated successfully!'
         data['success'] = True
         return JsonResponse(data, status=200)
@@ -100,18 +89,7 @@ def updateCompanyDetails(request, pk):
         return JsonResponse(data, status=400)
 
 
-
-
-
 def newCompanytPage(request):
-    """Render the page to Create a new company 
-
-    Arguments:
-        request {GET} -- [description]
-
-    Returns:
-        Html Page -- Renders the template to create a new company
-    """
     form_company = CompanyForm()
 
     context = {
@@ -121,17 +99,9 @@ def newCompanytPage(request):
 
 
 def postNewCompany(request):
-    """Saves the data of the new company
-
-    Arguments:
-        request {POST} -- Must be a Post request performed by ajax
-
-    Returns:
-        Json -- Returns Json data informing about the success or failure of the operation
-    """
     data = dict()
     if request.method == "POST" and request.is_ajax():
-        form_company = CompanyForm(request.POST)
+        form_company = CompanyForm(request.POST, request.FILES)
         form_company.save()
         data['message'] = 'Company created successfully'
         data['success'] = True
@@ -144,15 +114,6 @@ def postNewCompany(request):
 
 @csrf_protect
 def deleteCompany(request, pk):
-    """Deletes a copany by ajax
-
-    Arguments:
-        request {POST} -- Must be a Post request by ajax
-        pk {integer} -- Integer corresponding to the id of the company to be deleted
-
-    Returns:
-        Json -- Returns Json data informing about the success or failure of the operation
-    """
     if request.method == "POST" and request.is_ajax():
         data = dict()
         company = Company.objects.get(pk=pk)
@@ -165,14 +126,6 @@ def deleteCompany(request, pk):
 
 
 def newAddressPage(request):
-    """Returns the page to create a new address for a company
-
-    Arguments:
-        request {GET} -- [description]
-
-    Returns:
-        Html Page -- Html page that allows to add an address for a company
-    """
     form_address = CompanyAddressForm()
     context = {
         'company': company,
@@ -182,14 +135,6 @@ def newAddressPage(request):
 
 
 def postNewAddress(request):
-    """Records a new address at the database
-
-    Arguments:
-        request {POST} -- Must be a Post request by ajax
-
-    Returns:
-        Json -- Returns Json data informing about the success or failure of the operation
-    """
     data = dict()
     if request.method == "POST" and request.is_ajax():
         form_address = CompanyAddressForm(request.POST)
